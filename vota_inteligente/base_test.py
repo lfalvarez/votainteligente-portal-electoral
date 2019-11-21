318ed1b0b4060e76c05d73b32547e6db2ec5e168
Felipe Álvarez <luis.alvarez@thoughtworks.com>
2019-11-21 17:44:05 -0300

    initializing votai_utils

---
+++ vota_inteligente/base_test.py
@@ -0,0 +1,29 @@
+from django.test import TestCase, override_settings
+from PIL import Image
+from io import StringIO
+from django.core.files.base import ContentFile
+import random
+import os
+__dir__ = os.path.dirname(os.path.realpath(__file__))
+
+
+@override_settings(THEME=None)
+class VotaInteligenteTestCase(TestCase):
+    fixtures = ['mini_2.yaml']
+
+    def setUp(self):
+        super(VotaInteligenteTestCase, self).setUp()
+
+    def get_image(self):
+        image_file = StringIO()
+        color1 = random.randint(0, 255)
+        color2 = random.randint(0, 255)
+        color3 = random.randint(0, 255)
+        image = Image.new('RGBA', size=(50, 50), color=(color1, color2, color3))
+        image.save(image_file, 'png')
+        image_file.seek(0)
+        return ContentFile(image_file.read(), 'test.png')
+
+    def get_document(self):
+        pdf_file = open(__dir__ + '/fixtures/example.pdf')
+        return ContentFile(pdf_file.read(), 'example.pdf')
---
+++ votai_utils/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class VotaiUtilsConfig(AppConfig):
+    name = 'votai_utils'
