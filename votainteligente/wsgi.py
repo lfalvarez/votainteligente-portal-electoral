3336ff61496108e4e06a0a5382bd4c1e25d0f658
Felipe Álvarez <luis.alvarez@thoughtworks.com>
2019-11-22 12:16:09 -0300

    Cambiando nombre de l proyecto

--- votainteligente/wsgi.py
+++ votainteligente/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vota_inteligente.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'votainteligente.settings')
 
 application = get_wsgi_application()
